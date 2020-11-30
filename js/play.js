a33c96419aded5ba0405290c830bbab6f5a28001
minewet <71055964+minewet@users.noreply.github.com>
2020-11-30 14:07:10 +0900

    Update play.js

--- js/play.js
+++ js/play.js
@@ -2,7 +2,7 @@
 var player,
     time_update_interval = 0;
 
-var num=4;
+var num=5;
 var list=[
 ["불꽃놀이 (Remember Me)", "오마이걸", "https://cdnimg.melon.co.kr/cm/album/images/102/02/711/10202711_500.jpg?f6a4b682ad0d0799c509f22940434d7f/melon/quality/80/optimize", "UAshqNflYMY" ],
 
@@ -11,6 +11,10 @@ var list=[
 ["butterfly", "이달의 소녀", "https://cdnimg.melon.co.kr/cm/album/images/102/52/794/10252794_500.jpg?60cad57f0168f495cca7941da4fcdab5/melon/quality/80/optimize", "fvlbaW4YWf8"],
 
 ["밤하늘의 별을", "경서", "https://cdnimg.melon.co.kr/cm2/album/images/105/18/234/10518234_20201113150500_500.jpg?14229bd15eb93dec69341e7d2a01e9ab/melon/quality/80/optimize", "S_0me7vYyeU"],
+
+["5시 53분의 하늘에서 발견한 너와 나", "투모로우바이투게더",
+"https://cdnimg.melon.co.kr/cm2/album/images/105/08/871/10508871_20201026151152_500.jpg?76c663141007b35ed1bc482d96bd3de9/melon/quality/80/optimize",
+"NCMpDelurAk"]
 ];
 var playlist=list;
 var youtubelist=[];
@@ -20,8 +24,8 @@ for(var i=0;i<num;i++){
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('video-placeholder', {
-        width: 400,
-        height:300,
+        width: 1,
+        height:0,
         videoId: playlist[0][3],
         playerVars: {
             color: 'white',
@@ -39,7 +43,7 @@ function initialize(){
   updateProgressBar();
     player.cuePlaylist({playlist: youtubelist});
     player.setPlaybackQuality('small');
-
+    player.setVolume(50);
     // Update the controls on load
 
 
@@ -132,11 +136,17 @@ $('#play').on('click', function () {
 //볼륨창 조절
 $('#volume_bar').on('mouseup touchend', function(e) {
     var newVolume = e.target.value;
+    if(player.isMuted()){
+        player.unMute();
+    }
     player.setVolume(newVolume);
 });
 
-$('#volume_bar').on('mousemove', function(e) {
+$('#volume_bar').on('click mousemove', function(e) {
     var newVolume = e.target.value;
+    if(player.isMuted()){
+        player.unMute();
+    }
     player.setVolume(newVolume);
 }); //드래그도 바로 반영.
 
