d703ee2f56cedf6b60889ca0e43cf65911dd63b8
CODMILLI <pcy07111@naver.com>
2020-12-11 01:29:40 +0900

    edit

--- js/search.js
+++ js/search.js
@@ -2,10 +2,10 @@ function search_music() {
     const target = document.getElementById("search").value;
     if(target == '' || target == null) alert("검색 대상을 입력하세요.");
     else {
-        //window.open(
-        //    'https://www.youtube.com/results?search_query=' + target,
-        //    '_blank'
-        //);
+        window.open(
+            'https://www.youtube.com/results?search_query=' + target,
+            '_blank'
+        );
         location.href = 'find-music.html';
     }
 }
